f71f63c76a18881fd11a94d2be79df1a5dd4ed28
Kartik Thakur <kartikthakur029@gmail.com>
2023-12-03 20:08:33 +0530

    ReStructuring

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+import AdoptedPetContext from "./components/AdoptedPetContext";
+import Details from "./components/Details";
+import SearchParams from "./components/SearchParams";
+import "./styles/style.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
